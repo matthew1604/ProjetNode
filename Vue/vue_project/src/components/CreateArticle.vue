87b96659b29d866d5a59b1b642401add777b86b0
Benedicte <nasibenedicte@gmail.com>
2020-03-22 18:00:14 +0100

    front v2

--- Vue/vue_project/src/components/CreateArticle.vue
+++ Vue/vue_project/src/components/CreateArticle.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="form">
-    <b-form @submit="onSubmit" v-if="show">
+    <h2>Créer un article</h2>
+    <b-form @submit="onSubmit">
       <b-form-group id="input-group-1" label="Titre de l'article :" label-for="input-1">
         <b-form-input
           id="input-1"
-          v-model="form.title"
+          v-model="title"
           type="text"
           required
           placeholder="Entrez un titre"
@@ -13,33 +14,40 @@
       <b-form-group id="input-group-2" label="Contenu :" label-for="input-2">
           <b-form-textarea
             id="input-2"
-            v-model="form.description"
+            v-model="description"
             placeholder="Entrez votre texte"
             rows="3"
             max-rows="6"
         ></b-form-textarea>
       </b-form-group>
-      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="submit" variant="primary">Créer</b-button>
     </b-form>
   </div>
 </template>
 
 <script>
+import axios from "axios";
   export default {
     name: "CreateArticle",
     data() {
       return {
-        form: {
-          title: '',
-          description: ''
-        },
-        show: true
+        title: '',
+        description: ''
       }
     },
     methods: {
       onSubmit(evt) {
         evt.preventDefault()
-        alert(JSON.stringify(this.form))
+        axios
+        .post("https://projet-js-nasi-vergely.herokuapp.com/add-article", {
+          title: this.title,
+          body: this.description,
+          author_id: 1
+        })
+        .then(response => {
+          console.log(response.data)
+        })
+        .catch(error => console.log(error));
       }
     }
   }
@@ -49,5 +57,8 @@
 <style scoped>
 #form {
     margin: 5%;
+}
+h2 {
+  text-align: center;
 }
 </style>
--- Vue/vue_project/src/components/Home.vue
+++ Vue/vue_project/src/components/Home.vue
@@ -8,7 +8,10 @@
 
 <script>
 export default {
-  name: 'Home'
+  name: 'Home',
+  created() {
+    this.$store.commit("loadArticles");
+  }
 }
 </script>
 
